8e4d0f6ec3f01feab27f8d9ebac43fd8e02887d9
Ulises <uli.atx@gmail.com>
2019-08-03 17:52:02 -0500

    asset management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import _ from 'lodash'
+import './style.css'
+import Bernie from './bernieSanders.jpg'
+import Data from './democratic-candidate-totals-2019-08-03T17_37_29.csv'
+import xml from "./data.xml"
 
 const component = () => {
   const element = document.createElement('div');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Democratic', 'Presidential', 'Candidates'], ' ');
+  element.classList.add('title');
+
+  const bernieImg = new Image();
+  bernieImg.src = Bernie;
+
+  element.appendChild(document.createElement('br'));
+  element.appendChild(bernieImg);
+
+  console.log(xml);
+  console.log(Data);
 
   return element;
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,48 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        //webpack uses regex to determine which files it should look for and serve to a specific loader
+        //here files ending in .css are served to the style-loader and css-loader
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        //similar to above but this regex directs images to the file loader
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        loader: 'csv-loader',
+        options: {
+          dynamicTyping: true,
+          header: true,
+          skipEmptyLines: true
+        }
+      },
+      {
+        test: /\.xml$/,
+        use: [
+          'xml-loader'
+        ]
+      }
+    ]
   }
 };
